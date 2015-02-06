9e528d3b6373e069af0d7f5790ac196978a9f09c
Garrett <gsummers@calpoly.edu>
2015-02-05 21:48:26 -0800

    First commit for Lab 5 again

--- js/clock.js
+++ js/clock.js
@@ -3,6 +3,7 @@ window.onload = function() {
   getTemp();
 };
 
+
 function getTemp() {
    $.getJSON( "https://api.forecast.io/forecast/fed31c96894876831452babdf8d14d2b/35.300399,-120.662362?callback=?", function( result ) {
       $("#forecastLabel").html(result.daily.summary);
@@ -28,5 +29,35 @@ function getTime() {
    document.getElementById("time").innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
    setTimeout(function() {getTime();}, 1000);
    
+}
+
+
+function showAlarmPopup() {
+	$("#mask").removeClass("hide");
+	$("#popup").removeClass("hide");
+}
+
+function hideAlarmPopup() {
+	$("#mask").addClass("hide");
+	$("#popup").addClass("hide");
+}
+
+function insertAlarm (hours, mins, ampm, alarmName) {
+	var div = $("<div/>"), div2 = $("<div id='div2' class='name'/>"), div3 = $("<div id='div3' class='time'/>");
+	div.addClass("flexable");
+	div.append(div2, [div3]);
+	div2.html(alarmName);
+	div3.html(hours + ":" + mins + ampm); 
+	$("#alarms").append(div);
+}
+
+function addAlarm() {
+	var hours, mins, ampm, alarmName;
+	hours = $("#hours option:selected").text();
+	mins = $("#mins option:selected").text();
+	ampm = $("#ampm option:selected").text();
+	alarmName = $('#alarmName').val();
+	insertAlarm(hours, mins, ampm, alarmName);
+	hideAlarmPopup();
 }
 
